9fc0eabd9e0757f53b0ca8232dd5faecc073b651
Lorenzo Bracco <l.bracco@reply.it>
2015-10-28 14:33:16 +0100

    Fix system type

--- src/main/java/eu/vital/reply/services/HiPPI.java
+++ src/main/java/eu/vital/reply/services/HiPPI.java
@@ -118,7 +118,7 @@ public class HiPPI {
         // Context must point to a real file
         ioTSystem.setContext(contextsUri + "system.jsonld");
         ioTSystem.setId(system.getIoTSystem().getUri());
-        ioTSystem.setType("vital:IoTSystem"); // is it really it? Think so...
+        ioTSystem.setType("vital:VitalSystem"); // is it really it? Think so...
         ioTSystem.setName(system.getIoTSystem().getName());
         ioTSystem.setDescription(system.getIoTSystem().getDescription());
 
